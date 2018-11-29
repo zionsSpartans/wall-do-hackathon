d4a3a876b12eef0f09ee82ae05ea47b18055066e
Andoni A <andonialonsof@gmail.com>
2018-11-29 18:11:12 +0100

    Make output dict

--- walldo_py/walldo/elastic/queryAuth.py
+++ walldo_py/walldo/elastic/queryAuth.py
@@ -5,10 +5,12 @@ def queryauth(doc):
         ssh_event = doc['_source']['system']['auth']['ssh']['event']
         if  ssh_event == "Accepted":
             ssh_ip = doc['_source']['system']['auth']['ssh']['ip']
-            update_score(ssh_ip, -10)
+            detectedip = { "ip": ssh_ip, "score": -10 }
+            update_score(detectedip)
         elif ssh_event == "Failed":
             ssh_ip = doc['_source']['system']['auth']['ssh']['ip']
-            update_score(ssh_ip, 20)
+            detectedip = { "ip": ssh_ip, "score": 20 }
+            update_score(detectedip)
     except:
         print("")
 
