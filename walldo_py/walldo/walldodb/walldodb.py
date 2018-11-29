4413b47991b5a7ca1f974265dab9647fa77cb87f
Andoni A <andonialonsof@gmail.com>
2018-11-29 18:16:17 +0100

    WIP: walldodb.py

--- walldo_py/walldo/walldodb/walldodb.py
+++ walldo_py/walldo/walldodb/walldodb.py
@@ -5,23 +5,21 @@ bd = conn()
 score = bd.puntuaciones
 
 # Objetos temporales para pruebas
-detectedip = { "ip": "10.0.13.1", "score": 35 }
-badip = {"ip": "10.0.13.1", "score": 10}
-niceip = {"ip": "10.0.13.1", "score": -5}
+#detectedip = { "ip": "10.0.13.1", "score": 35 }
+#badip = {"ip": "10.0.13.1", "score": 10}
+#niceip = {"ip": "10.0.13.1", "score": -5}
 
 
 
-def update_score(detectedip):
+def update_score(ip_frommodule):
   # Recuperamos info de BBDD
-  ip_indb = score.find_one({"ip": detectedip["ip"]})
-  new_score = ip_indb["score"] + detectedip["score"]
+  ip_indb = score.find_one({"ip": ip_frommodule["ip"]})
+  new_score = ip_indb["score"] + ip_frommodule["score"]
   print(ip_indb)
-  score.update_one({"ip": detectedip["ip"]}, { "$set": { "score": new_score }})
-  print(score.find_one({"ip": detectedip["ip"]}))
+  # Actualizamos en BBDD con la nueva puntuación
+  score.update_one({"ip": ip_frommodule["ip"]}, { "$set": { "score": new_score }})
+  print(score.find_one({"ip": ip_frommodule["ip"]}))
 
 
 
 
-
-### Testing funciones
-insert_score(detectedip)
