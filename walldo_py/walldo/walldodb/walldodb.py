d51956ff1acef0b4fb27ee332cc3aac9648f81f6
Andoni A <andonialonsof@gmail.com>
2018-11-29 16:53:00 +0100

    WIP: walldodb.py

--- walldo_py/walldo/walldodb/walldodb.py
+++ walldo_py/walldo/walldodb/walldodb.py
@@ -9,11 +9,14 @@ detectedip = {"ip": "10.0.13.1", "score": 35}
 badip = {"ip": "10.0.13.1", "score": 10}
 niceip = {"ip": "10.0.13.1", "score": -5}
 
-def insert_score(detectedip):
+# Insert simple
+def simple_insert_score(detectedip):
   # Insert del documento en la tabla puntuaciones
   score.insert_one(detectedip)
 
-def
+def update_score(detectedip):
+  ip_tofind = detectedip[2]
+  score.find_one()
 
 
 
