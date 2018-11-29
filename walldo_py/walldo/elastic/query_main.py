e28663beec8cfffb5fb9a1e7617dbb09d70b59e2
ravigon2 <rafa_lizo@hotmail.com>
2018-11-29 18:39:52 +0100

    wip

--- walldo_py/walldo/elastic/query_main.py
+++ walldo_py/walldo/elastic/query_main.py
@@ -15,11 +15,9 @@ while (scroll_size > 0):
     for doc in res['hits']['hits']:
         if doc['_source']['source'] == "/var/log/auth.log":
             queryauth(doc)
-    # es.delete(index="filebeat*", doc_type="doc", id=doc['_id'])
+        es.delete(index="filebeat*", doc_type="doc", id=doc['_id'])
 
-    # print("%s) %s" % (doc['_id'], doc['_source']['system']))
-
-    # time.sleep(5)
+    time.sleep(5)
     res = es.search(index="filebeat*", body={"query": {"match_all": {}}})
 
-# elasticsearch.helpers.reindex(client=elasticSource, source_index="filebeat-6.4.2-2018.11.24", target_index="indice_principal", target_client=elasticDestination)
+#elasticsearch.helpers.reindex(client=elasticSource, source_index="filebeat-6.4.2-2018.11.24", target_index="indice_principal", target_client=elasticDestination)
