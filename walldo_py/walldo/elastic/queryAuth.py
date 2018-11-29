bb976ca3af679049665c7583bf947c080e7e1949
ravigon2 <rafa_lizo@hotmail.com>
2018-11-29 17:39:33 +0100

    try/except

--- walldo_py/walldo/elastic/queryAuth.py
+++ walldo_py/walldo/elastic/queryAuth.py
@@ -1,9 +1,15 @@
 from elasticsearch import Elasticsearch
 
 def queryauth(doc):
-    print(doc['_source']['system'])
+    #print(doc['_source']['system'])
 
-    #if ssh_event is None:
+    try:
+        if  doc['_source']['system']['auth']['ssh']['event'] == "Accepted":
+            print("Accepted")
+        if doc['_source']['system']['auth']['ssh']['event'] == "Failed":
+            print("Failed")
+    except Exception as e:
+        print(e)
     #    print("Vacio")
     #if  doc['_source']['system']['auth']['ssh']['event'] == "Accepted":
     #    print("Accepted")
