a52ee5ab27c1ca43a6e277a2a24fb8386378eb2a
Andoni A <andonialonsof@gmail.com>
2018-11-30 17:16:32 +0100

    External vars

---
+++ walldo_py/walldo/global_conf.py
@@ -0,0 +1,15 @@
+
+
+
+
+##########################################
+#
+# Configuración puntuaciones /var/log/auth
+#
+##########################################
+# Resta de puntos por contraseña aceptada
+ssh_accepted_score = -10
+# Suma de puntos por contraseña erronea
+ssh_failed_score = 25
+# Suma de puntos por contraseña erronea con root
+ssh_failed_root_score = 20
