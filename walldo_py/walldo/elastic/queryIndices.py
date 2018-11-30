7784ec3e283484546b1a2cbf859881d9c3ec30f2
ravigon2 <rafa_lizo@hotmail.com>
2018-11-30 11:49:46 +0100

    wip

--- walldo_py/walldo/elastic/queryIndices.py
+++ walldo_py/walldo/elastic/queryIndices.py
@@ -1,8 +1,13 @@
 from elasticconfig import es
 
+i = 0
+
 def queryIndices():
-    var = es.indices.get_alias("file*")
-    sorted(var)
-    indice = list(var.keys())[0]
-    res = es.search(index=indice, body={"query": {"match_all": {}}})
-    return indice
+    var = sorted(es.indices.get_alias("file*"))
+    while i < len(var):
+        indice = var[i]
+        res = es.search(index=indice, body={"query": {"match_all": {}}})
+        if res['hits']['total'] > 0:
+            print(indice)
+            return indice
+        ++i
