c29fb13c0186b5b09a5e375c7415a4b235b9a969
ravigon2 <rafa_lizo@hotmail.com>
2018-11-30 13:17:20 +0100

    Se contempla errores con user root

--- walldo_py/walldo/elastic/queryAuth.py
+++ walldo_py/walldo/elastic/queryAuth.py
@@ -20,14 +20,17 @@ from walldodb import update_score
 def queryauth(doc):
     try:
         ssh_event = doc['_source']['system']['auth']['ssh']['event']
+        ssh_user = doc['_source']['system']['user']
+        ssh_ip = doc['_source']['system']['auth']['ssh']['ip']
         if  ssh_event == "Accepted":
-            ssh_ip = doc['_source']['system']['auth']['ssh']['ip']
             detectedip = { "ip": ssh_ip, "score": -10 }
             print("Patron Accepted detectado")
             update_score(detectedip)
         elif ssh_event == "Failed":
-            ssh_ip = doc['_source']['system']['auth']['ssh']['ip']
-            detectedip = { "ip": ssh_ip, "score": 20 }
+            if ssh_user == "root":
+                detectedip = { "ip": ssh_ip, "score": 20 }
+            else:
+                detectedip = {"ip": ssh_ip, "score": 25}
             print("Patron Failed detectado")
             update_score(detectedip)
     except:
