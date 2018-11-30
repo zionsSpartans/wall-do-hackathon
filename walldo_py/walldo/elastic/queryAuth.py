904aedfeb48ac9277d5d03cae2209e08951c237d
Andoni A <andonialonsof@gmail.com>
2018-11-30 12:30:47 +0100

    Add prints query auth

--- walldo_py/walldo/elastic/queryAuth.py
+++ walldo_py/walldo/elastic/queryAuth.py
@@ -23,10 +23,12 @@ def queryauth(doc):
         if  ssh_event == "Accepted":
             ssh_ip = doc['_source']['system']['auth']['ssh']['ip']
             detectedip = { "ip": ssh_ip, "score": -10 }
+            print("Patron Accepted detectado")
             update_score(detectedip)
         elif ssh_event == "Failed":
             ssh_ip = doc['_source']['system']['auth']['ssh']['ip']
             detectedip = { "ip": ssh_ip, "score": 20 }
+            print("Patron Accepted detectado")
             update_score(detectedip)
     except:
         print("Patron no detectado")
