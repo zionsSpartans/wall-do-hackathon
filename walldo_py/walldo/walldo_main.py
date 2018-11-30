90e4474e32486e290a5b83b4f9e440736747dc4d
Andoni A <andonialonsof@gmail.com>
2018-11-30 16:48:37 +0100

    Fix delay on walldo to w8 elastic

--- walldo_py/walldo/walldo_main.py
+++ walldo_py/walldo/walldo_main.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
 import sys
 sys.path.append('/walldo')
-
 from elastic.query_main import query_main
+
+# Espera para permitir que Elastic arranque
+# PDTE de sustituir por un healtcheck a una URL de elastic
+print("Esperando 60 segundos para arrancar...")
+time.sleep(60)
+print("Arrancando!")
 
 while True:
     query_main()
