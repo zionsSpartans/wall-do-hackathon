25252790ede3e62252667c6428b7a819cf854bf7
Andoni A <andonialonsof@gmail.com>
2018-11-30 19:30:10 +0100

    WIP ssh

--- walldo_py/walldo/acciones/ssh.py
+++ walldo_py/walldo/acciones/ssh.py
@@ -7,7 +7,7 @@ import time
 hosts_path = "/walldo/hosts"
 
 # Ejemplo de dict para pruebas
-# [{'ip': 12, 'tiempo': '120', 'cd': '180'}]
+# {'ip': "14.14.14.14", 'tiempo': '60', 'cd': '180'}
 
 # Funcion que a traves de un script auxiliar banea la IP que se le pasa.
 #
@@ -16,6 +16,7 @@ hosts_path = "/walldo/hosts"
 def accion_baneo_ssh(ssh_dict):
     ip_toban = ssh_dict["ip"]
     baneo = ssh_dict["tiempo"]
+    cooldown = ssh_dict["cd"]
     # Ejecucion del script auxiliar
     torun = subprocess.Popen(["run_ban_ssh.sh", hosts_path, ip_toban], stdout=subprocess.PIPE)
     output , err = torun.communicate()
