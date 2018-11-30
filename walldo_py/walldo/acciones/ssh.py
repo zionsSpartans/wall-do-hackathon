5bb5cb0c9bbcc1672252315c75a18a674a0952a0
Andoni A <andonialonsof@gmail.com>
2018-11-30 19:09:53 +0100

    WIP ssh

--- walldo_py/walldo/acciones/ssh.py
+++ walldo_py/walldo/acciones/ssh.py
@@ -1,5 +1,5 @@
 import subprocess
-
+import datetime
 
 # PDTE utilizar directamente python con ansible API
 
@@ -8,10 +8,14 @@ hosts_path = "/walldo/hosts"
 # Funcion que a traves de un script auxiliar banea la IP que se le pasa.
 #
 # Ademas, insert en una tabla la fecha del baneo y la de desbaneo para poder llevar un control.
+#
 def accion_baneo_ssh(ssh_dict):
     ip_toban = ssh_dict["ip"]
+    # Ejecucion del script auxiliar
     torun = subprocess.Popen(["run_ban_ssh.sh", hosts_path, ip_toban], stdout=subprocess.PIPE)
-    output , err = p.communicate()
+    output , err = torun.communicate()
+    # Print output ejecucion ansible
     print(output)
-
+    # Guardamos fecha del baneo
+    ban_timestamp = datetime.datetime.fromtimestamp(1543508331).isoformat()
 
