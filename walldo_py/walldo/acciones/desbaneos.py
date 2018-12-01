6c9db2d1ac40ab8cc9f938df9c12f2af33ca3dbe
ravigon2 <rafa_lizo@hotmail.com>
2018-12-01 12:22:39 +0100

    Revision desbaneos

--- walldo_py/walldo/acciones/desbaneos.py
+++ walldo_py/walldo/acciones/desbaneos.py
@@ -1,6 +1,7 @@
 from walldodb.configbd import conn
 from acciones.ssh import desbaneo_ssh
-import time
+from walldodb.whitelist import is_white
+from walldodb.walldodb_main import update_score
 import datetime
 
 # Batch temporal a ejecutar en segundo plano mientras se investiga como trabajar con trigger en mongo
@@ -13,7 +14,14 @@ baneos = bd.baneos
 def revisa_baneos():
     coleccion = baneos.find()
     for doc in coleccion:
-        if doc['unban'] < datetime.datetime.now():
-            ip = doc['ip']
-            print(ip)
+        ip = doc['ip']
+        if is_white(ip):
+            # Poner a 0
+            print("IP en la whitelist, poniendo a 0: " + str(ip))
+            ip_toreduce = {"ip": ip, "score": 0}
+            update_score(ip_toreduce)
+            # Eliminar entrada de la coleccion de baneos
+            baneos.delete_one({"ip": ip})
+            desbaneo_ssh(ip)
+        elif doc['unban'] < datetime.datetime.now():
             desbaneo_ssh(ip)
