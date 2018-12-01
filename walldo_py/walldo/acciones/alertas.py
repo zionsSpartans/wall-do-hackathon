ead3729774f34cfada4f27492dcfbf02462fa45e
ravigon2 <rafa_lizo@hotmail.com>
2018-12-01 09:23:49 +0100

    Modificaciones acciones

--- walldo_py/walldo/acciones/alertas.py
+++ walldo_py/walldo/acciones/alertas.py
@@ -4,8 +4,8 @@ def alert(ip, puntuaciones):
     i = 0
     while i < len(alertas):
         if str(puntuaciones) < alertas[i]['rango']:
-            alert = [{'ip': ip}, {'tiempo': alertas[i]['tiempo']}]
-            return alert
+            alerta = [{'ip': ip}, {'tiempo': alertas[i]['tiempo']}]
+            return alerta
         i += 1
 
     return 0
