7b425dfd41b7b03dc3ff9113544fec3d07324860
Andoni A <andonialonsof@gmail.com>
2018-12-01 10:17:44 +0100

    WIP ssh

---
+++ notas/scripts_testing/aciertaSSH.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+hostname=$1
+
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
+sshpass -p 'estanoeslapass' ssh vagrant@${hostname} 'id'
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
+sshpass -p 'estanoeslapass' ssh vagrant@${hostname} 'id'
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
+sshpass -p 'estanoeslapass' ssh vagrant@${hostname} 'id'
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
+sshpass -p 'vagrant' ssh vagrant@${hostname} 'id'  # Esta si es la pass ;)
