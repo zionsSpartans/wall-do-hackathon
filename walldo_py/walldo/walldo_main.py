c674aa205eb509663f2bd9cabb5e6538fa68a66a
ravigon2 <rafa_lizo@hotmail.com>
2018-12-01 11:24:03 +0100

    Revision desbaneos

--- walldo_py/walldo/walldo_main.py
+++ walldo_py/walldo/walldo_main.py
@@ -9,6 +9,7 @@ from global_conf import whitelist_path
 from walldodb.whitelist import load_whitelist
 from elastic.query_main import query_main
 from acciones.acciones_main import recoje_puntuaciones
+from acciones.desbaneos import revisa_baneos
 
 def proceso1():
     while True:
@@ -19,14 +20,22 @@ def proceso2():
         recoje_puntuaciones()
         time.sleep(180)
 
+def proceso3():
+    while True:
+        revisa_baneos()
+        time.sleep(180)
+
 
 def main():
     p1 = Process(target=proceso1)
     p1.start()
     p2 = Process(target=proceso2)
     p2.start()
+    p3 = Process(target=proceso3)
+    p3.start()
     p1.join()
     p2.join()
+    p3.join()
 
 if __name__ == '__main__':
     # Espera para permitir que Elastic arranque
